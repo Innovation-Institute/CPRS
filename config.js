d080a9dc7dc65c8f539badcdf8d8f774f4d03184
Hoshang Charania <hok14@pitt.edu>
2018-11-14 15:08:22 -0500

    fixed untracked files and gitignore

--- config.js
+++
@@ -1,9 +0,0 @@
-var Airtable = require('airtable');
-Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: 'keym5eCUcyBiIupFJ'
-});
-var base = Airtable.base('apptesMGdnPGkPzGE');
-module.exports= {
-    base : base
-}
