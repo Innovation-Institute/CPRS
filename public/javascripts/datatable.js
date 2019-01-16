dae486b8995bb4d2ea94792e9a7ed45801096ddb
Hoshang Charania <hok14@pitt.edu>
2019-01-16 17:25:55 -0500

    Added option to select specific columns from datatable

---
+++ public/javascripts/datatable.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    var table=$('#example').DataTable({
+      dom: 'Bfrtip',
+      /**
+       * Choose which columns you don't wish to show in the datatable.
+       * Either by class name such as 'last-column' or by position such as 0
+       * is the first position, similar to an indexed array.
+       */
+      columnDefs: [
+            {
+                targets: 'last-column',
+                className: 'noVis'
+            },
+            {
+                targets: 0,
+                className: 'noVis'
+            },
+
+        ],
+        /**
+         * extend-> Functionality -> Make sure to see if the appropriate js file for the functionality is loaded.
+         * text -> Name on button
+         * exportOptions -> specifications for export, right now I've said 
+         *                  don't include the last th element as that is 
+         *                  the Actions field. We don't want the view and 
+         *                  edit button in the exported report.
+         */
+      buttons: [
+   {
+        extend: 'copy',
+        text: 'Copy Table',
+        exportOptions: {
+            columns: 'th:not(:last-child)'
+        }
+    },{
+        extend: 'csv',
+        text: 'Export CSV',
+        exportOptions: {
+            columns: 'th:not(:last-child)'
+        }
+    },{
+        extend: 'excel',
+        text: 'Export Excel',
+        exportOptions: {
+            columns: 'th:not(:last-child)'
+        }
+    },{
+        extend: 'pdf',
+        text: 'Export PDF',
+        exportOptions: {
+            columns: 'th:not(:last-child)'
+        }
+    },{
+        extend: 'print',
+        text: 'Print',
+        exportOptions: {
+            columns: 'th:not(:last-child)'
+        }
+    },{
+            extend: 'colvis',
+            text: 'Choose Columns',
+            columns: ':not(.noVis)'
+            }],
+    });
+    table.columns('.not-visible').visible( false );
+  });
--- routes/reports.js
+++ routes/reports.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const ReportsController = require('../controllers/ReportsController');
+const Reports = require('../controllers/ReportsController');
+
+const ReportsController= new Reports();
 
 /**
  * Index
